d75181a94dd74b394f10648197d8181da7901cb7
Sebastian Zawisza <anszau@gmail.com>
2019-02-01 13:33:23 +0100

    añadido order by a selectAll de categoria, y solucionado bug en scrollspy

--- model/Categoria.php
+++ model/Categoria.php
@@ -57,7 +57,7 @@ class Categoria
 
     public static function getAll()
     {
-        return connection()->query("SELECT * FROM Categoria")->fetchAll();
+        return connection()->query("SELECT * FROM Categoria ORDER BY preferencia")->fetchAll();
     }
 
     /**
